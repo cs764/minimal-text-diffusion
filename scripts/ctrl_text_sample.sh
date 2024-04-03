2f0a6825ce139e106ab38ea4625f2491103518b2
cs764 <165441060+cs764@users.noreply.github.com>
2024-04-03 03:15:12 -0400

    Update ctrl_text_sample.sh

--- scripts/ctrl_text_sample.sh
+++ scripts/ctrl_text_sample.sh
@@ -21,7 +21,7 @@ CLAMP=${7:-no_clamp}
 SEQ_LEN=${8:-10}
 SEED=${9:-10708}
 
-python -u src/train_infer/controllable_text_sample.py --model_name_or_path ${MODEL_NAME} \
+python -u src/controllable/controllable_text_sample.py --model_name_or_path ${MODEL_NAME} \
 --batch_size ${BATCH_SIZE} --num_samples ${NUM_SAMPLES} --top_p ${TOP_P} \
 --seed ${SEED} \
 --out_dir ${OUT_DIR} --diffusion_steps ${DIFFUSION_STEPS} --clamp ${CLAMP} --sequence_len ${SEQ_LEN}
